bd49deb4d27d02eb6b2cf2fb3cbc876d67f5f5c7
alojua <juan.alonso@staempfli.com>
2016-08-09 00:38:46 +0200

    requirejs template

---
+++ templates/jsrequire/view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * require-config.js
+ *
+ * @copyright Copyright (c) ${generator.time.year} ${comments.company.name}
+ * @author    ${comments.user.mail}
+ */
+var config = {
+    map: {
+        '*': {
+            ${Identifier}: '${Vendorname}.${Modulename}/js/${identifier}'
+        }
+    }
+};
---
+++ templates/jsrequire/view/frontend/web/js/${identifier}.js
@@ -0,0 +1,47 @@
+/**
+ * ${identifier}
+ *
+ * @copyright Copyright (c) ${generator.time.year} ${comments.company.name}
+ * @author    ${comments.user.mail}
+ */
+
+/*jshint jquery:true*/
+define([
+    "jquery",
+    "jquery/ui"
+], function ($) {
+    "use strict";
+
+    $.widget('${vendorname}.${modulename}.${identifier}', {
+        options: {
+            // optionName: value
+        },
+
+        /**
+         * Bind a click handler on the widget's element.
+         * @private
+         */
+        _create: function() {
+            this.element.on('click', $.proxy(this._clickAction, this));
+        },
+
+        /**
+         * Init object
+         * @private
+         */
+        _init: function () {
+            // Do something if needed
+        },
+
+        /**
+         * Click action function
+         * @private
+         * @param event - {Object} - Click event.
+         */
+        _clickAction: function(event) {
+            // Do something with element clicked $(event.target)
+        }
+    });
+
+    return $.${vendorname}.${modulename}.${identifier};
+});
