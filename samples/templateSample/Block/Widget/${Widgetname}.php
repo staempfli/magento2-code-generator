eaa96abf89b1d96b96a570114d0da5a48e9d9971
alojua <juan.alonso@staempfli.com>
2016-10-02 13:29:23 +0200

    Updating documentation and adding sample template

---
+++ samples/templateSample/Block/Widget/${Widgetname}.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ${Widgetname}
+ *
+ * @copyright Copyright (c) ${generator.time.year} ${comments.company.name}
+ * @author    ${comments.user.mail}
+ */
+
+namespace ${Vendorname}\${Modulename}\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class ${Widgetname} extends Template implements BlockInterface
+{
+    /**
+     * @var string $_template
+     */
+    protected $_template = "widget/${widgetname}.phtml";
+
+    // write your methods here...
+}
