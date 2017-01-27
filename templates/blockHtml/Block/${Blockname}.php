52cca81cf3901718ec83c00f71c352f46fa5ea16
alojua <juan.alonso@staempfli.com>
2017-01-27 13:52:50 +0100

    New template blockHtml

---
+++ templates/blockHtml/Block/${Blockname}.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ${Blockname}
+ *
+ * @copyright Copyright (c) ${commentsYear} ${CommentsCompanyName}
+ * @author    ${commentsUserEmail}
+ */
+
+namespace ${Vendorname}\${Modulename}\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class ${Blockname} extends Template
+{
+    // write your methods here...
+}
