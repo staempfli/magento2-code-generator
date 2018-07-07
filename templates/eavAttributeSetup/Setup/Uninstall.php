2fbaa2669508d1c0eafed9e367b76d8699d293ae
Wilfried Wolf <wilfried.wolf@sandstein.de>
2018-07-07 18:47:47 +0200

    corrected setup interface class

--- templates/eavAttributeSetup/Setup/Uninstall.php
+++ templates/eavAttributeSetup/Setup/Uninstall.php
@@ -11,7 +11,7 @@ namespace ${Vendorname}\${Modulename}\Setup;
 
 use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\UninstallInterface;
-use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 
 class Uninstall implements UninstallInterface
@@ -38,7 +38,7 @@ class Uninstall implements UninstallInterface
      * {@inheritdoc}
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context) //@codingStandardsIgnoreLine
+    public function uninstall(ModuleDataSetupInterface $setup, ModuleContextInterface $context) //@codingStandardsIgnoreLine
     {
         $setup->startSetup();
 
